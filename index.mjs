6bf61ca4ab9aeb5fe361f465c15328306906337b
lindon0390 <60849618+lindon0390@users.noreply.github.com>
2025-02-13 12:50:48 +0700

    first

---
+++ index.mjs
@@ -0,0 +1,8 @@
+import 'zx/globals';
+import 'path';
+import 'process';
+import 'fs-extra';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+$.verbose = true; //prints all executed commands alongside with their outputs
