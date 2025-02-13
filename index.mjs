2914122bb109073b64873cf2c8b82ccd9b329c8a
lindon0390 <60849618+lindon0390@users.noreply.github.com>
2025-02-13 13:01:49 +0700

    init

--- index.mjs
+++ index.mjs
@@ -6,3 +6,17 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 $.verbose = true; //prints all executed commands alongside with their outputs
+
+const dirPath = process.env.PATH_REPOS ?? '';
+
+function getDirectories(sourcePath) {
+    return fs.readdirSync(sourcePath)
+      .filter(item => fs.statSync(path.join(sourcePath, item)).isDirectory());
+  }
+  
+  try {
+    const directories = getDirectories(dirPath);
+    console.log(directories);
+  } catch (error) {
+    console.error("Ошибка:", error.message);
+  }
